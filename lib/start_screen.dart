559380dff348ed704089eedb9c16d8e8e60a630e
AskarKadir <askarkadir07@gmail.com>
2023-03-30 20:54:39 +0700

    make button pda halaman start-screen to login_page

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:exercise_satu/login_page.dart';
 
 class StartScreen extends StatelessWidget {
   const StartScreen({super.key});
@@ -9,21 +10,28 @@ class StartScreen extends StatelessWidget {
         // force image fit screen
         body: Container(
             decoration: const BoxDecoration(
-                image: DecorationImage(
-      image: AssetImage('assets/images/start-screen.png'),
-      fit: BoxFit.cover,
-      //   ),
-      // ),
-      // child: Column(
-      //   mainAxisAlignment: MainAxisAlignment.center,
-      //   children: [
-      //     // FloatingActionButton.small(onPressed: (){
-      //     //   Navigator.push(context,
-      //     //   MaterialPageRoute(
-      //     //     builder: (context) => const DetailPage()));
-      //     // })
-      //   ],
-      // ))
-    ))));
+              image: DecorationImage(
+                image: AssetImage('assets/images/start-screen.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: Column(children: [
+              Expanded(
+                  child: Align(
+                alignment: Alignment.bottomCenter,
+                child: Container(
+                    margin: EdgeInsets.only(bottom: 50),
+                    child: FloatingActionButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const LoginPage()));
+                      },
+                      child: const Icon(Icons.arrow_forward),
+                      backgroundColor: Color.fromRGBO(255, 215, 2, 1),
+                    )),
+              ))
+            ])));
   }
 }
