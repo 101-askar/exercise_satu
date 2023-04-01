b12e9a000a611535b3018ca1c4ad988c71940d8b
AskarKadir <askarkadir07@gmail.com>
2023-04-02 05:38:20 +0700

    form register with valitdator

--- lib/register.dart
+++ lib/register.dart
@@ -5,13 +5,142 @@ class RegisterPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final _formKey = GlobalKey<FormState>();
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Register'),
-      ),
-      body: Center(
-        child: Text('Register'),
-      ),
-    );
+        resizeToAvoidBottomInset: false,
+        appBar: AppBar(
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back, color: Color.fromRGBO(255, 215, 2, 1)),
+            onPressed: () => Navigator.of(context).pop(),
+          ),
+          title: Text("P O K E D E X",
+              style: TextStyle(
+                color: Color.fromRGBO(255, 215, 2, 1),
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              )),
+          centerTitle: true,
+          backgroundColor: Color.fromRGBO(100, 36, 44, 1),
+        ),
+        body: Stack(
+          alignment: Alignment.center,
+          children: <Widget>[
+            Container(
+              /* background */
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("assets/images/register-screen.png"),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 200),
+              alignment: Alignment.center,
+              width: 300,
+              height: 400,
+              // color: Colors.black38,
+              child: Form(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        hintText: 'Enter your username',
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.white,
+                          ),
+                          borderRadius: BorderRadius.all(Radius.circular(15)),
+                        ),
+                      ),
+                      validator: (String? value) {
+                        if (value!.isEmpty) {
+                          return "* Required";
+                        } else if (value.length < 6) {
+                          return "Password should be atleast 6 characters";
+                        } else if (value.length > 15) {
+                          return "Password should not be greater than 15 characters";
+                        } else
+                          return null;
+                      },
+                    ),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        hintText: 'Enter your username',
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.white,
+                          ),
+                          borderRadius: BorderRadius.all(Radius.circular(15)),
+                        ),
+                      ),
+                      validator: (String? value) {
+                        if (value!.isEmpty) {
+                          return "* Required";
+                        } else if (value.length < 6) {
+                          return "Password should be atleast 6 characters";
+                        } else if (value.length > 15) {
+                          return "Password should not be greater than 15 characters";
+                        } else
+                          return null;
+                      },
+                    ),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        hintText: 'Enter your username',
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.white,
+                          ),
+                          borderRadius: BorderRadius.all(Radius.circular(15)),
+                        ),
+                      ),
+                      validator: (String? value) {
+                        if (value!.isEmpty) {
+                          return "* Required";
+                        } else if (value.length < 6) {
+                          return "Password should be atleast 6 characters";
+                        } else if (value.length > 15) {
+                          return "Password should not be greater than 15 characters";
+                        } else
+                          return null;
+                      },
+                    ),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        hintText: 'Enter your username',
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.white,
+                          ),
+                          borderRadius: BorderRadius.all(Radius.circular(15)),
+                        ),
+                      ),
+                      validator: (String? value) {
+                        if (value!.isEmpty) {
+                          return "* Required";
+                        } else if (value.length < 6) {
+                          return "Password should be atleast 6 characters";
+                        } else if (value.length > 15) {
+                          return "Password should not be greater than 15 characters";
+                        } else
+                          return null;
+                      },
+                    ),
+                    ElevatedButton(
+                        onPressed: () {},
+                        child: Text("Register"),
+                        style: ElevatedButton.styleFrom(
+                            primary: Color.fromRGBO(255, 215, 2, 1),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ))),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ));
   }
 }
