5fad76624c5a8a0d0c092730f4db12413d43c9b3
AskarKadir <askarkadir07@gmail.com>
2023-04-02 03:42:24 +0700

    fix layout halaman login

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -8,132 +8,138 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final _formKey = GlobalKey<FormState>();
     return Scaffold(
-      // create login form
-      body: Container(
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('assets/images/login-screen.png'),
-              fit: BoxFit.cover,
+        /* fixed background move when keyboard appears */
+        resizeToAvoidBottomInset: false,
+        body: Stack(
+          children: <Widget>[
+            Container(
+              // image background
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/login-screen.png'),
+                  fit: BoxFit.cover,
+                ),
+              ),
             ),
-          ),
-          child: Container(
-            alignment: AlignmentDirectional.center,
-            child: Column(
-              children: [
-                Expanded(
-                  child: Align(
-                    child: Container(
-                      margin: EdgeInsets.only(bottom: 50),
-                      child: Form(
-                        key: GlobalKey<FormState>(),
+            Center(
+              child: Container(
+                margin: EdgeInsetsDirectional.only(top: 300),
+                width: 320,
+                height: 450,
+                color: Colors.white38,
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          hintText: 'Enter your email',
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Colors.white,
+                            ),
+                            borderRadius: BorderRadius.all(Radius.circular(15)),
+                          ),
+                        ),
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'Username tidak boleh kosong';
+                          }
+                          return null;
+                        },
+                      ),
+                      Padding(
+                          padding:
+                              EdgeInsetsDirectional.only(bottom: 30, top: 30)),
+                      TextFormField(
+                        obscureText: true,
+                        decoration: const InputDecoration(
+                          hintText: 'Enter your password',
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Colors.white,
+                            ),
+                            borderRadius: BorderRadius.all(Radius.circular(15)),
+                          ),
+                        ),
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'Please enter some text';
+                          }
+                          return null;
+                        },
+                      ),
+                      Container(
+                        width: 320,
+                        height: 50,
+                        color: Colors.black26,
+                        margin: EdgeInsetsDirectional.only(top: 100),
                         child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
+                          children: <Widget>[
                             Container(
-                              margin: EdgeInsets.only(bottom: 20, top: 350),
-                              child: TextFormField(
-                                decoration: InputDecoration(
-                                  hintText: 'Username',
-                                  hintStyle: TextStyle(
-                                    color: Colors.grey,
+                                width: 320,
+                                child: ElevatedButton(
+                                  onPressed: () {/* validate */},
+                                  child: Text(
+                                    'Login',
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold),
                                   ),
-                                  border: OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(10),
-                                    borderSide: BorderSide(
-                                      color: Colors.white,
+                                  style: ElevatedButton.styleFrom(
+                                    primary: Color.fromRGBO(255, 215, 2, 1),
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10),
                                     ),
                                   ),
-                                  enabledBorder: OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(10),
-                                    borderSide: BorderSide(
-                                      color: Colors.white,
-                                    ),
-                                  ),
-                                  focusedBorder: OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(10),
-                                    borderSide: BorderSide(
-                                      color: Colors.white,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ),
-                            Container(
-                              margin: EdgeInsets.only(bottom: 20),
-                              child: TextFormField(
-                                decoration: InputDecoration(
-                                  hintText: 'Password',
-                                  hintStyle: TextStyle(
-                                    color: Colors.grey,
-                                  ),
-                                  border: OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(10),
-                                    borderSide: BorderSide(
-                                      color: Colors.white,
-                                    ),
-                                  ),
-                                  enabledBorder: OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(10),
-                                    borderSide: BorderSide(
-                                      color: Colors.white,
-                                    ),
-                                  ),
-                                  focusedBorder: OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(10),
-                                    borderSide: BorderSide(
-                                      color: Colors.white,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ),
-                            Container(
-                              margin: EdgeInsets.only(bottom: 20),
-                              child: ElevatedButton(
-                                onPressed: () {
-                                  // validate logic
-                                },
-                                child: const Text('Login'),
-                                style: ElevatedButton.styleFrom(
-                                  primary: Color.fromRGBO(255, 215, 2, 1),
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(10),
-                                  ),
-                                ),
-                              ),
-                            ),
-                            Container(
-                                alignment: Alignment.topCenter,
-                                child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    Text('Don\'t have an account?'),
-                                    TextButton(
-                                      onPressed: () {
-                                        // navigate to register page
-                                        Navigator.push(
-                                            context,
-                                            MaterialPageRoute(
-                                                builder: (context) =>
-                                                    RegisterPage()));
-                                      },
-                                      child: Text('Register'),
-                                      style: TextButton.styleFrom(
-                                        primary: Color.fromRGBO(255, 215, 2, 1),
-                                      ),
-                                    ),
-                                  ],
-                                )),
+                                ))
                           ],
                         ),
                       ),
-                    ),
+                      Container(
+                        width: 320,
+                        height: 50,
+                        color: Colors.blueGrey,
+                        margin: EdgeInsetsDirectional.only(top: 30),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              'Don\'t have an account?',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 15,
+                              ),
+                            ),
+                            TextButton(
+                              onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => RegisterPage()),
+                                );
+                              },
+                              child: Text(
+                                'Register',
+                                style: TextStyle(
+                                  color: Color.fromRGBO(255, 215, 2, 1),
+                                  fontSize: 15,
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-              ],
+              ),
             ),
-          )),
-    );
+          ],
+        ));
   }
 }
