98f0253b290e2b6813761f13362988f81fc96be8
AskarKadir <askarkadir07@gmail.com>
2023-04-02 06:16:56 +0700

    email validation format

--- lib/register.dart
+++ lib/register.dart
@@ -78,12 +78,23 @@ class RegisterPage extends StatelessWidget {
                         ),
                       ),
                       validator: (String? value) {
+                        const pattern =
+                            r"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'"
+                            r'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-'
+                            r'\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*'
+                            r'[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4]'
+                            r'[0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9]'
+                            r'[0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\'
+                            r'x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])';
+                        final regex = RegExp(pattern);
                         if (value!.isEmpty) {
                           return "* Required";
                         } else if (value.length < 6) {
                           return "Password should be atleast 6 characters";
                         } else if (value.length > 15) {
                           return "Password should not be greater than 15 characters";
+                        } else if (!regex.hasMatch(value)) {
+                          "Enter a valid email";
                         } else
                           return null;
                       },
